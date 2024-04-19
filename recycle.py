4e30a86e9ba56cdd2c69bdc6a518d741981ddc86
Andreianghel11 <andreianghel2011@gmail.com>
2024-04-19 10:35:39 +0300

    Implemented GPU training

--- recycle.py
+++ recycle.py
@@ -9,24 +9,25 @@ from matplotlib import pyplot as plt
 #also shuffles the data
 #data = tf.keras.utils.image_dataset_from_directory('data')
 
-import cv2
-import imghdr
+# Eliminates bad data
+# import cv2
+# import imghdr
 
-data_dir = 'data' 
-image_exts = ['jpeg','jpg', 'bmp', 'png']
+# data_dir = 'data' 
+# image_exts = ['jpeg','jpg', 'bmp', 'png']
 
-for image_class in os.listdir(data_dir): 
-    for image in os.listdir(os.path.join(data_dir, image_class)):
-        image_path = os.path.join(data_dir, image_class, image)
-        try: 
-            img = cv2.imread(image_path)
-            tip = imghdr.what(image_path)
-            if tip not in image_exts: 
-                print('Image not in ext list {}'.format(image_path))
-                os.remove(image_path)
-        except Exception as e: 
-            print('Issue with image {}'.format(image_path))
-            #os.remove(image_path)
+# for image_class in os.listdir(data_dir): 
+#     for image in os.listdir(os.path.join(data_dir, image_class)):
+#         image_path = os.path.join(data_dir, image_class, image)
+#         try: 
+#             img = cv2.imread(image_path)
+#             tip = imghdr.what(image_path)
+#             if tip not in image_exts: 
+#                 print('Image not in ext list {}'.format(image_path))
+#                 os.remove(image_path)
+#         except Exception as e: 
+#             print('Issue with image {}'.format(image_path))
+#             #os.remove(image_path)
 
 print(tf.__version__)
 print(keras.__version__)
@@ -102,7 +103,7 @@ model.compile(
 model.fit(
   train_ds,
   validation_data=val_ds,
-  epochs=2
+  epochs=20
 )
 
 
--- test.py
+++ test.py
@@ -1,25 +1,6 @@
 import tensorflow as tf
 
-dataset = tf.data.Dataset.range(100)
+print(tf.config.list_physical_devices('GPU'))
 
-train_size = 20
-valid_size = 30
-test_size = 50
-
-train = dataset.take(train_size)
-remaining = dataset.skip(train_size)
-valid = remaining.take(valid_size)
-test = remaining.skip(valid_size)
-
-for i in train:
-    print(i)
-
-print("\n\n")
-
-for i in valid:
-    print(i)
-
-print("\n\n")
-
-for i in test:
-    print(i)
+# tf.test.is_built_with_cuda()
+# tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
