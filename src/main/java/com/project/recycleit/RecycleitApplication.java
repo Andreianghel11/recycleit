ab56294f82784b7f2bce44081bf4ecaaa4fad93c
Andreianghel11 <andreianghel2011@gmail.com>
2024-04-19 11:53:05 +0300

    Initial commit

---
+++ src/main/java/com/project/recycleit/RecycleitApplication.java
@@ -0,0 +1,13 @@
+package com.project.recycleit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/recycleit/RecycleitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recycleit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
